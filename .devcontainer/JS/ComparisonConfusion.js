b601eb1b081b784f1b95338ba56a49f3edde7ee4
Ravi sahani <108450291+Ravicsecap@users.noreply.github.com>
2024-02-02 15:52:15 +0000

    NuLL and Undefined

---
+++ .devcontainer/JS/ComparisonConfusion.js
@@ -0,0 +1,16 @@
+
+// NOTE:- equality( == ) and Comparision(>=,<=) both are work different //
+// i.e. null value consider "0" if Comparision//
+//i.e.  undifind value compare its retrun false //
+// if compare any value note down datatype must be same just bcus accuracy rate high.//
+
+console.log(null == 0);  //false
+console.log(null >= 0);  //true
+console.log(null <= 0);  // true
+
+
+
+console.log(undefined == 0);   //false
+
+console.log(undefined >= 0);//false
+console.log(undefined <= 0);//false
