ab475f2afc62360972d0984b18b84b06894bae5a
Ravi sahani <108450291+Ravicsecap@users.noreply.github.com>
2024-02-02 13:00:54 +0000

    container config done

---
+++ .devcontainer/JS/Variable.js
@@ -0,0 +1,24 @@
+ // avoid to use "VAR" key word  just bcus its a function scope //
+ // let used for props will be changing in futur and re-declare //
+// const used for only value cant be change and both are block scope //
+
+// 1)---- WE TAKE I/P BY USER AND JS VARIABLE KEYWORD IS MOST POWERFULL JUST BCUS THESE ARE HOLD OBJECT AND EXPERSSION//
+
+const Acc_no = `12345@OOvhw`;
+
+let name = "Ravi Sahani";
+
+phone_no = 9702457526;
+
+alternate_no = 9867846694;
+
+var city = "Mumbai";
+
+  city =  `${city},S'CRUZ - WEST`;
+
+  state = `MAHARASHTRA `;
+
+  let address;
+
+  console.table([Acc_no. name,phone_no,alternate_no,city,state,address]);
+
