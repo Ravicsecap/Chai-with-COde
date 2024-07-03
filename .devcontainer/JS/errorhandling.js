50df395ba0442da506c5a4dd65073a81666fc443
Ravishekhar Sahani <108450291+Ravicsecap@users.noreply.github.com>
2024-07-04 02:20:08 +0530

    Error Handling

---
+++ .devcontainer/JS/errorhandling.js
@@ -0,0 +1,18 @@
+
+
+//... try , catch  and finally block ...//
+
+
+try {
+    console.log('hello, i am new user');
+    // console.log(x);
+    console.log('please login me');
+
+} catch (error) {
+
+    console.log('wait for some mintue somthing will be wrong');
+    console.log(error);
+    
+}finally{
+    console.log('sometime after referce ur internet ');
+}
