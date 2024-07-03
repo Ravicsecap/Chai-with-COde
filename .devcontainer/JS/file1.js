e07d53cf85978815077f9c36cada43ca73c7c973
Ravishekhar Sahani <108450291+Ravicsecap@users.noreply.github.com>
2024-07-04 02:18:21 +0530

    Modules

---
+++ .devcontainer/JS/file1.js
@@ -0,0 +1,14 @@
+
+export const addtion = (a , b )=> {
+    return a+b;
+}
+
+export const subtraction = ( a,b) => {
+
+    return a-b;
+}
+
+export default multiply=(a , b)=>{
+
+    return a*b;
+}
---
+++ .devcontainer/JS/file2.js
@@ -0,0 +1,21 @@
+
+import { addtion , subtraction as sub, random_name } from "./file1.js";
+
+
+
+
+const a = 54;
+const b = 40;
+
+const addresult = addtion(a,b)
+
+console.log(addresult);
+
+
+const subresult = sub(a,b);
+
+console.log(subresult);
+
+const multiplyresult = random_name(a, b)
+
+console.log(multiplyresult);
